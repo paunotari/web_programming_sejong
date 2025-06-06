efcfc5b3decfa1ba104cbc9aade414df6fa09408
Pau Notari <135167158+paunotari@users.noreply.github.com>
2025-06-06 10:09:40 +0000

    Dynamic Routing Working Pau

--- server.js
+++ server.js
@@ -4,29 +4,31 @@ const path = require('path');
 const app = express();
 const PORT = 3000;
 
-// Serve static files (CSS, JS, images)
-app.use(express.static(path.join(__dirname, 'public')));
+// Serve static assets from custom folders
+app.use('/css', express.static(path.join(__dirname, 'css')));
+app.use('/js', express.static(path.join(__dirname, 'javascript')));
+app.use('/images', express.static(path.join(__dirname, 'images')));
 
 // Routes
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'views', 'index.html'));
+  res.sendFile(path.join(__dirname, 'home.html'));
 });
 
 app.get('/about', (req, res) => {
-  res.sendFile(path.join(__dirname, 'views', 'about.html'));
+  res.sendFile(path.join(__dirname, 'about_us.html'));
 });
 
 app.get('/products', (req, res) => {
-  res.sendFile(path.join(__dirname, 'views', 'products.html'));
+  res.sendFile(path.join(__dirname, 'products.html'));
 });
 
 app.get('/contact', (req, res) => {
-  res.sendFile(path.join(__dirname, 'views', 'contact.html'));
+  res.sendFile(path.join(__dirname, 'contact.html'));
 });
 
-// 404 Fallback
+// 404 Page
 app.use((req, res) => {
-  res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+  res.status(404).sendFile(path.join(__dirname, '404.html'));
 });
 
 app.listen(PORT, () => {
